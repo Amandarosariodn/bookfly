5231090578424d2a27f9158ad0828ff57dc08890
Amandarosariodn <amandaredes15@gmail.com>
2026-05-23 16:25:35 -0300

    fix: ajuste no map de estante

--- bookfly.Infra/Estantes/Mappings/EstanteMap.cs
+++ bookfly.Infra/Estantes/Mappings/EstanteMap.cs
@@ -1,21 +1,21 @@
 
-using bookfly.Domain.Estantes.Entities;
-using FluentNHibernate.Mapping;
+    using bookfly.Domain.Estantes.Entities;
+    using FluentNHibernate.Mapping;
 
-namespace bookfly.Infra.Estantes.Mappings
-{
-    public class EstanteMap : ClassMap<Estante>
+    namespace bookfly.Infra.Estantes.Mappings
     {
-        public EstanteMap()
+        public class EstanteMap : ClassMap<Estante>
         {
-            Table("estante");
-            Id(Estante => Estante.Id).Column("id").GeneratedBy.Identity();
-            Map(Estante => Estante.UsuarioId).Column("usuario_id").Not.Nullable();
-            Map(Estante => Estante.Nome).Column("nome");
-            Map(Estante => Estante.Descricao).Column("descricao").Nullable();
-            Map(Estante => Estante.Privada).Column("privada").CustomType<bool>().Nullable();
-            Map(Estante => Estante.CriadoEm).Column("criado_em").Nullable();
+            public EstanteMap()
+            {
+                Table("estante");
+                Id(Estante => Estante.Id).Column("id").GeneratedBy.Identity();
+                References(Estante => Estante.UsuarioId).Column("usuario_id").Not.Nullable();
+                Map(Estante => Estante.Nome).Column("nome");
+                Map(Estante => Estante.Descricao).Column("descricao").Nullable();
+                Map(Estante => Estante.Privada).Column("privada").CustomType<bool>().Nullable();
+                Map(Estante => Estante.CriadoEm).Column("criado_em").Nullable();
 
+            }
         }
     }
-}
