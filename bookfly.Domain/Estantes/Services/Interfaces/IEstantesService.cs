ad0dfa29924213b48198d76c73e8187f960ba40c
Amandarosariodn <amandaredes15@gmail.com>
2026-05-23 15:32:57 -0300

    BFLY-27-Ajusta estante service

--- bookfly.Domain/Estantes/Services/Interfaces/IEstantesService.cs
+++ bookfly.Domain/Estantes/Services/Interfaces/IEstantesService.cs
@@ -1,16 +1,18 @@
+using bookfly.Domain.Estantes.Commands;
 using bookfly.Domain.Estantes.Entities;
+using bookfly.Domain.Estantes.Repositories.Filters;
 
 namespace bookfly.Domain.Estantes.Services.Interfaces
 {
     public interface IEstantesService
     {
-        Task<Estante> InserirEstanteAsync(InserirCategoriaCommand comando, CancellationToken cancellationToken);
+        Task<Estante> InserirEstanteAsync(InserirEstanteCommand comando, CancellationToken cancellationToken);
         Task<Estante> ValidarAsync(int id, CancellationToken cancellationToken);
 
-        Task<Estante> EditarEstanteAsync(EditarCategoriaCommand comando, int id, CancellationToken cancellationToken);
-        Estante Instanciar(InserirCategoriaCommand comando);
+        Task<Estante> EditarEstanteAsync(EditarEstanteCommand comando, int id, CancellationToken cancellationToken);
+        Estante Instanciar(InserirEstanteCommand comando);
         Task MudarSituacaoAsync(int id, CancellationToken cancellationToken);
 
-        Task<List<Estante>> ListarAsync(CategoriaFiltro categoria, CancellationToken cancellationToken);
+        Task<List<Estante>> ListarAsync(EstanteFiltro categoria, CancellationToken cancellationToken);
     }
 }
