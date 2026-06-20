0138d89784ef1e3b78199a95696326308de2e306
Amandarosariodn <amandaredes15@gmail.com>
2026-06-19 23:19:41 -0300

    fix: adiciona campo usuarioid no filtro de avaliacao

--- bookfly.Domain/Avaliacoes/Repositories/Filters/AvaliacaoFiltro.cs
+++ bookfly.Domain/Avaliacoes/Repositories/Filters/AvaliacaoFiltro.cs
@@ -4,5 +4,6 @@ namespace bookfly.Domain.Avaliacoes.Repositories.Filters
     public class AvaliacaoFiltro
     {
         public int? LivroId { get; set; }
+        public int? UsuarioId { get; set; }
     }
 }
