ee8b0e3cc169d14c6705f54bd6f7ac0f95b8eeb0
Amandarosariodn <amandaredes15@gmail.com>
2026-06-28 02:36:59 -0300

    feat: implementa camada infra de comunidade

---
+++ bookfly.Infra/Comunidades/ComunidadeRepository.cs
@@ -0,0 +1,46 @@
+using bookfly.Domain.Comunidades.Entities;
+using bookfly.Domain.Comunidades.Repositories.Filters;
+using bookfly.Domain.Comunidades.Repositories.Interfaces;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace bookfly.Infra.Comunidades
+{
+    public class ComunidadeRepository(ISession session) : IComunidadeRepository
+    {
+        public async Task EditarAsync(Comunidade comunidade, CancellationToken cancellationToken)
+        {
+            if (comunidade == null)
+                throw new ArgumentNullException(nameof(comunidade));
+            await session.UpdateAsync(comunidade, cancellationToken);
+        }
+
+        public IQueryable<Comunidade> Filtrar()
+        {
+            return session.Query<Comunidade>();
+
+        }
+
+        public async Task InserirAsync(Comunidade comunidade, CancellationToken cancellationToken)
+        {
+            if (comunidade == null)
+                throw new ArgumentNullException(nameof(comunidade));
+            await session.SaveAsync(comunidade, cancellationToken);
+        }
+
+        public async Task<List<Comunidade>> ListarAsync(ComunidadeFiltro comunidade, CancellationToken cancellationToken)
+        {
+            var query = session.Query<Comunidade>();
+
+            if (!string.IsNullOrEmpty(comunidade.Nome))
+                query = query.Where(c => c.Nome.Contains(comunidade.Nome));
+
+            return await query.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Comunidade?> RecuperarPorIdAsync(int comunidadeId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ bookfly.Infra/Comunidades/Mappings/ComunidadeMap.cs
@@ -0,0 +1,24 @@
+
+
+using bookfly.Domain.Comunidades.Entities;
+using FluentNHibernate.Mapping;
+
+namespace bookfly.Infra.Comunidades.Mappings
+{
+    public class ComunidadeMap : ClassMap<Comunidade>
+    {
+        public ComunidadeMap()
+        {
+            Table("comunidade");
+
+            Id(comunidade => comunidade.Id).Column("ID").GeneratedBy.Identity();
+            Map(comunidade => comunidade.CriadorId).Column("criador_id").Nullable();
+            Map(comunidade => comunidade.Nome).Column("nome").Nullable();
+            Map(comunidade => comunidade.Descricao).Column("descricao").Nullable();
+            Map(comunidade => comunidade.UrlImagem).Column("url_imagem").Nullable();
+            Map(comunidade => comunidade.Privado).Column("privada").Nullable();
+            Map(comunidade => comunidade.Ativo).Column("ativo_inativo").Nullable();
+            Map(comunidade => comunidade.DataCriacao).Column("criado_em").Nullable();
+        }
+    }
+}
